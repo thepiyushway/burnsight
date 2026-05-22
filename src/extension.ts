a03568469f9cf8efebbd417ee274da56c3844362
Piyush Sharma <thepiyushway@gmail.com>
2026-05-23 04:20:35 +0530

    Initialize BurnSight extension runtime and status bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "burnsight" is now active!');
+  statusBarItem.text = '🧠 $0.00';
+  statusBarItem.tooltip = 'BurnSight';
+  statusBarItem.command = 'burnsight.openOverlay';
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('burnsight.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from burnsight!');
-	});
+  statusBarItem.show();
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(statusBarItem);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
